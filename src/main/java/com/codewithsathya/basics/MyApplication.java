f74e720c6d3777104d06bd25a748ca015e76b02d
sathyasn46 <sathyasn46@gmail.com>
2024-10-13 00:12:09 +0530

    strings explained

--- src/main/java/com/codewithsathya/basics/MyApplication.java
+++ src/main/java/com/codewithsathya/basics/MyApplication.java
@@ -14,7 +14,7 @@ public class MyApplication {
 //		1. Fundamentals
 			//Types
 			explainingTypes();
-					
+			explainingStrings();
 			//Control flows
 			
 			//package the application
@@ -28,10 +28,29 @@ public class MyApplication {
 		
 	}
 	
+	private static void explainingStrings() {
+		System.out.println("\n===== explainingStrings=====");
+		String  msg = "Sathya learns Java Basics";
+		System.out.println(msg);
+		
+		
+		System.out.println(msg.endsWith("Basics"));
+		System.out.println(msg.indexOf("earn"));
+		System.out.println(msg);
+		System.out.println(msg.replace('a', 'o'));
+		System.out.println(msg);
+		//Original string is not changed . Strings are immutable. We cannot mutate/change them
+		
+//		Parameters are fields present in the method definition
+//		Arguments are actual values passed to the methods
+		
+		System.out.println("\n===== explainingStrings Exit=====");
+		
+	}
+
 	static void explainingTypes() {
 		
 		primitiveTypes();
-		
 		referenceTypes();
 		primitiveVsReferenceType();
 	}
@@ -44,6 +63,10 @@ public class MyApplication {
 		Integer primIt = 5;
 		Date joiningDate = new Date();
 		System.out.println(joiningDate);
+		
+		//Reference types has members which can be accessed using  . DOT operator
+		
+		
 		System.out.println("\n===== Reference types End=====");
 	}
 
@@ -56,6 +79,7 @@ public class MyApplication {
 		float highPrice = 1000_000.99F;
 		char letter = 's';
 		boolean isDeveloper = true;
+		
 		
 		//Primitive Types - for storing simple values
 //		TYPE	BYTES	RANGE
@@ -78,13 +102,17 @@ public class MyApplication {
 		byte y = x; 
 		// y will not be affetced if value of x changes
 		// x an y are completely independent
+		//Primitive types are copied by their value
 		
-		Point point1 = new Point(1,1);
+		Point point1 = new Point(1,1);// Assume point(1,1) is stored in address 100
 		Point point2 = point1;
 		System.out.println(point1);
 		System.out.println(point2);
 		//Point1 points to address of Point(1,1)
-//		point2 points to the address
+		//in Point1 memory location -stores address of point object i.e 100
+//		point 1 and 2 are referencing the exact same object in the memory
+		//Not independent, so changes will be impacted
+		//Reference types are copied by their references
 		
 		System.out.println("=====primitiveVsReferenceType End=====");
 	}
